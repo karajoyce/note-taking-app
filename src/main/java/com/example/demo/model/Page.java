4a53c517cb9fafd8ad1e125bde5a6308f3cc84f1
Kara Leier <kjl061@usask.ca>
2024-11-22 12:18:53 -0600

    working on integration and pages

--- src/main/java/com/example/demo/model/Page.java
+++ src/main/java/com/example/demo/model/Page.java
@@ -2,6 +2,17 @@ package com.example.demo.model;
 
 import org.fxmisc.richtext.InlineCssTextArea;
 
+/**
+
+ CMPT 370, T05, Team 4, Prof. Jon Lovering
+ Author: Kara Leier, kjl061, 11293306
+ Nathan Balilis, ncb421, 11295020
+ Trushank Lakdawala, nus429, 11350445
+ Jinny Kim, yek738, 11304174
+ Sara Shakeel, gvk731, 11367521
+
+ **/
+
 public class Page {
 
     private String title;
