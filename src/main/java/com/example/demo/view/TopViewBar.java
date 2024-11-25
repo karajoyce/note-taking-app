69694de846e7dd7eb4090509d369d376a353a1ae
Trushank Lakdawala <trushankwork@gmail.com>
2024-11-25 01:30:53 -0600

    Trying something

--- src/main/java/com/example/demo/view/TopViewBar.java
+++ src/main/java/com/example/demo/view/TopViewBar.java
@@ -4,10 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 
 public class TopViewBar extends HBox {
 
